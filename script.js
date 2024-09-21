14d2b540328448b9265741df298a6bea6d8143cd
毛泽东 <146086032+0000017@users.noreply.github.com>
2024-09-21 19:14:18 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// 获取按钮和显示消息的元素
-const button = document.getElementById('clickMeButton');
-const message = document.getElementById('message');
-
-// 为按钮添加点击事件
-button.addEventListener('click', function() {
-  message.textContent = '你点击了按钮！';
-  button.style.backgroundColor = 'green';  // 点击后按钮变为绿色
-});
